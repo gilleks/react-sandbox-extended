3ef6c5c1af83b6dee11c507f1db4d5f6d799aa59
VladislavZhukov <zhukov.vlad89@gmail.com>
2024-04-14 07:25:31 +0400

    GILL-22: update css loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -23,6 +23,8 @@ export function buildLoaders(
                         localIdentName: isDev
                             ? '[path][name]__[local]--[hash:base64:5]'
                             : '[hash:base64:8]',
+                        namedExport: false,
+                        exportLocalsConvention: 'as-is',
                     },
                 },
             },
